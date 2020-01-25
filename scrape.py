0d896f4aa32da0f0822d815a5572a2a4e85404cb
Ali Goldani <ali.goldani@ymail.com>
2020-01-26 01:24:53 +0330

    A function saved the code from redundancy

--- scrape.py
+++ scrape.py
@@ -4,57 +4,31 @@ from bs4 import BeautifulSoup
 from openpyxl import Workbook
 from openpyxl import load_workbook
 
-SPREADSHEET_NAME = 'test.xlsx'
+SPREADSHEET_NAME = 'test3.xlsx'
+
+
+def fetch_price_from(url):
+    soup = BeautifulSoup(requests.get(url).text, "html.parser")
+    return(soup.select_one(
+        '#main > div.profile-container.container > div:nth-child(2) > ul > li:nth-child(1) > span'
+    ).text)
 
 
 if __name__ == '__main__':
     # Fetching currencies
-    soup = BeautifulSoup(requests.get('https://www.tgju.org/chart/price_dollar_rl').text, "html.parser")
-    dollar_price = soup.select_one(
-        '#main > div.profile-container.container > div:nth-child(2) > ul > li:nth-child(1) > span'
-    ).text
-
-    soup = BeautifulSoup(requests.get('http://www.tgju.org/chart/price_eur').text, "html.parser")
-    euro_price = soup.select_one(
-        '#main > div.profile-container.container > div:nth-child(2) > ul > li:nth-child(1) > span'
-    ).text
-
-    soup = BeautifulSoup(requests.get('http://www.tgju.org/chart/price_aed').text, "html.parser")
-    dirham_price = soup.select_one(
-        '#main > div.profile-container.container > div:nth-child(2) > ul > li:nth-child(1) > span'
-    ).text
-
-    soup = BeautifulSoup(requests.get('http://www.tgju.org/chart/price_cny').text, "html.parser")
-    yuan_price = soup.select_one(
-        '#main > div.profile-container.container > div:nth-child(2) > ul > li:nth-child(1) > span'
-    ).text
+    dollar_price = fetch_price_from('https://www.tgju.org/chart/price_dollar_rl')
+    euro_price = fetch_price_from('https://www.tgju.org/chart/price_eur')
+    dirham_price = fetch_price_from('https://www.tgju.org/chart/price_aed')
+    yuan_price = fetch_price_from('https://www.tgju.org/chart/price_cny')
 
     # Fetching petrol prices
-    soup = BeautifulSoup(requests.get('http://www.tgju.org/chart/energy-crude-oil').text, "html.parser")
-    crude_price = soup.select_one(
-        '#main > div.profile-container.container > div:nth-child(2) > ul > li:nth-child(1) > span'
-    ).text
-
-    soup = BeautifulSoup(requests.get('http://www.tgju.org/chart/energy-brent-oil').text, "html.parser")
-    brent_price = soup.select_one(
-        '#main > div.profile-container.container > div:nth-child(2) > ul > li:nth-child(1) > span'
-    ).text
-
-    soup = BeautifulSoup(requests.get('http://www.tgju.org/chart/oil_opec').text, "html.parser")
-    opec_price = soup.select_one(
-        '#main > div.profile-container.container > div:nth-child(2) > ul > li:nth-child(1) > span'
-    ).text
-
-    soup = BeautifulSoup(requests.get('http://www.tgju.org/chart/energy-heating-oil').text, "html.parser")
-    mazut_price = soup.select_one(
-        '#main > div.profile-container.container > div:nth-child(2) > ul > li:nth-child(1) > span'
-    ).text
+    crude_price = fetch_price_from('https://www.tgju.org/chart/energy-crude-oil')
+    brent_price = fetch_price_from('https://www.tgju.org/chart/energy-brent-oil')
+    opec_price = fetch_price_from('https://www.tgju.org/chart/oil_opec')
+    mazut_price = fetch_price_from('https://www.tgju.org/chart/energy-heating-oil')
 
     # Fetching global gold prices
-    soup = BeautifulSoup(requests.get('http://www.tgju.org/chart/ons').text, "html.parser")
-    gold_dollars = soup.select_one(
-        '#main > div.profile-container.container > div:nth-child(2) > ul > li:nth-child(1) > span'
-    ).text
+    gold_dollars = fetch_price_from('https://www.tgju.org/chart/ons')
 
     # Writing the scraped data into a spreadsheet
     scraped_data = [datetime.datetime.now().date(), dollar_price, euro_price, dirham_price, yuan_price, crude_price, brent_price, opec_price, mazut_price, gold_dollars]
@@ -70,7 +44,9 @@ if __name__ == '__main__':
 
         wb.save(SPREADSHEET_NAME)
         print('Data added to ' + SPREADSHEET_NAME)
-    except FileNotFoundError:   # Spreadsheet does not exist in the root folder, creating a new one and appending to it
+
+    # Spreadsheet does not exist in the root folder, creating a new one and appending to it
+    except FileNotFoundError:
         wb = Workbook()
         ws = wb.active
         ws.append(['Datetime', 'Dollar Price', 'Euro Price', 'Dirham Price', 'Yuan Price', 'Crude Price', 'Brent Price', 'Opec Price', 'Mazut Price', 'Global Gold Price ($)'])
